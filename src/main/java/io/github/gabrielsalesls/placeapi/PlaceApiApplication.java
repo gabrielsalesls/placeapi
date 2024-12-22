9562ca5582d55c9882e61c1bb93ff23ebd5f8820
gabrielsalesls <salesgabriel311@gmail.com>
2024-12-22 19:02:02 -0300

    refactor: adjust package name to follow naming conventions

--- src/main/java/io/github/gabrielsalesls/placeapi/PlaceApiApplication.java
+++ src/main/java/io/github/gabrielsalesls/placeapi/PlaceApiApplication.java
@@ -1,4 +1,4 @@
-package io.github.gabrielsalesls.placeAPI;
+package io.github.gabrielsalesls.placeapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
