cb2ee44cd9f5923c6ffd25f6e6a9890bbb34377d
gabrielsalesls <salesgabriel311@gmail.com>
2024-12-22 18:22:05 -0300

    feature: base project

---
+++ src/main/java/io/github/gabrielsalesls/placeAPI/PlaceApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.gabrielsalesls.placeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gabrielsalesls/placeAPI/PlaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabrielsalesls.placeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
