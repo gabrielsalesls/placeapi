de2debdb6276eefd6d31f89eb3d4e4c3ed73e8be
gabrielsalesls <salesgabriel311@gmail.com>
2024-12-23 19:31:45 -0300

    feature: error handling for IllegalArgumentException

---
+++ src/main/java/io/github/gabrielsalesls/placeapi/exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,20 @@
+package io.github.gabrielsalesls.placeapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    ProblemDetail handleIllegalArgument(IllegalArgumentException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage());
+        problemDetail.setProperty("TimeStamp", Instant.now());
+        return problemDetail;
+    }
+}
